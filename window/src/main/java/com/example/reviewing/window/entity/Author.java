d175a738f7e11ff8104a14a8cd9cb562f4ac6129
JT Antoine JAN20882 <JAN20882@open-groupe.com>
2026-06-03 17:45:48 +0200

    [JTA/JAE] : Ajout entités

---
+++ window/src/main/java/com/example/reviewing/window/entity/Author.java
@@ -0,0 +1,63 @@
+package com.example.reviewing.window.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String type;
+
+    @OneToMany(mappedBy = "author")
+    private List<Review> reviews;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+}
