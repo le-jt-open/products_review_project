56eaf24024fb84c27c49ad8ea151045bb7692374
JT Antoine JAN20882 <JAN20882@open-groupe.com>
2026-06-04 18:41:00 +0200

    [JTA/JAE] : Ajout des couches pour les avis

---
+++ window/src/main/java/com/example/reviewing/window/service/ReviewService.java
@@ -0,0 +1,10 @@
+package com.example.reviewing.window.service;
+
+public interface ReviewService {
+
+    /**
+     * Returns the total number of reviews.
+     * @return
+     */
+    long countReviews();
+}
