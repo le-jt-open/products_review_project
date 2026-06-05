85005bd8fd5a81c0900411a5231d64a851677b61
JT Antoine JAN20882 <JAN20882@open-groupe.com>
2026-06-05 19:58:21 +0200

    [JTA/JAE] : Correction conf et code poru bon fonctionnement

--- window-frontend/src/main.ts
+++ window-frontend/src/main.ts
@@ -1,5 +1,13 @@
 
+import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideZoneChangeDetection } from '@angular/core';
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent,{
+    providers: [
+        provideHttpClient(),
+        provideZoneChangeDetection()
+    ]
+});
